a9869a29a2cf45131908b591746995a048c21010
Alberto Nasello <alberto@levelapp.be>
2024-02-27 20:09:38 +0100

    Add production history configuration

--- webapp/src/router/index.ts
+++ webapp/src/router/index.ts
@@ -14,6 +14,7 @@ import NetworkAdminView from '@/views/NetworkAdminView.vue';
 import NetworkInfoView from '@/views/NetworkInfoView.vue';
 import NtpAdminView from '@/views/NtpAdminView.vue';
 import NtpInfoView from '@/views/NtpInfoView.vue';
+import ProductionHistoryView from '@/views/ProductionHistoryView.vue';
 import SecurityAdminView from '@/views/SecurityAdminView.vue';
 import SystemInfoView from '@/views/SystemInfoView.vue';
 import { createRouter, createWebHistory } from 'vue-router';
@@ -61,6 +62,11 @@ const router = createRouter({
         path: '/info/console',
         name: 'Web Console',
         component: ConsoleInfoView
+    },
+    {
+      path: '/settings/production-history',
+      name: 'Production history',
+      component: ProductionHistoryView
     },
     {
         path: '/settings/network',
---
+++ webapp/src/views/ProductionHistoryView.vue
@@ -0,0 +1,46 @@
+<template>
+    <BasePage :title="$t('menu.ProductionHistory')" :isLoading="dataLoading">
+        <CardElement :text="$t('productionhistoryadmin.urlConfiguration')" textVariant="text-bg-primary">
+          <InputElement :label="$t('productionhistoryadmin.fetchingUrl')" type="text" v-model="url"/>
+        </CardElement>
+        <FormFooter @reload="null"/>
+    </BasePage>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BasePage from '@/components/BasePage.vue';
+import CardElement from '@/components/CardElement.vue';
+import { authHeader, handleResponse } from '@/utils/authentication';
+import FormFooter from '@/components/FormFooter.vue';
+import InputElement from '@/components/InputElement.vue';
+
+export default defineComponent({
+    components: {
+        BasePage,
+        CardElement,
+        FormFooter,
+        InputElement,
+    },
+    data() {
+        return {
+            dataLoading: false,
+            url: "",
+        };
+    },
+    created() {
+        this.getHistoryUrl();
+    },
+    methods: {
+        getHistoryUrl() {
+            // this.dataLoading = true;
+            // fetch("/api/history-url", { headers: authHeader() })
+            //     .then((response) => handleResponse(response, this.$emitter, this.$router))
+            //     .then((data) => {
+            //         this.ntpDataList = data;
+            //         this.dataLoading = false;
+            //     });
+        },
+    },
+});
+</script>
