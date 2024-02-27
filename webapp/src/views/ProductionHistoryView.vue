17c6bcff4035a86dd5c976c62e748b7bbb75e526
Alberto Nasello <alberto@levelapp.be>
2024-02-27 20:56:49 +0100

    Add production history view post

--- webapp/src/views/ProductionHistoryView.vue
+++ webapp/src/views/ProductionHistoryView.vue
@@ -1,9 +1,14 @@
 <template>
-    <BasePage :title="$t('menu.ProductionHistory')" :isLoading="dataLoading">
+  <BasePage :title="$t('menu.ProductionHistory')" :isLoading="dataLoading">
+    <BootstrapAlert v-model="showAlert" dismissible :variant="alertType">
+      {{ alertMessage }}
+    </BootstrapAlert>
+    <form @submit="postHistoryUrl">
         <CardElement :text="$t('productionhistoryadmin.urlConfiguration')" textVariant="text-bg-primary">
-          <InputElement :label="$t('productionhistoryadmin.fetchingUrl')" type="text" v-model="url"/>
+          <InputElement placeholder="https://" :label="$t('productionhistoryadmin.fetchingUrl')" type="text" v-model="url"/>
         </CardElement>
-        <FormFooter @reload="null"/>
+        <FormFooter />
+      </form>
     </BasePage>
 </template>
 
@@ -14,6 +19,7 @@ import CardElement from '@/components/CardElement.vue';
 import { authHeader, handleResponse } from '@/utils/authentication';
 import FormFooter from '@/components/FormFooter.vue';
 import InputElement from '@/components/InputElement.vue';
+import BootstrapAlert from "@/components/BootstrapAlert.vue";
 
 export default defineComponent({
     components: {
@@ -21,25 +27,49 @@ export default defineComponent({
         CardElement,
         FormFooter,
         InputElement,
+        BootstrapAlert,
     },
     data() {
         return {
             dataLoading: false,
             url: "",
+
+            alertMessage: "",
+            alertType: "info",
+            showAlert: false,
         };
     },
     created() {
         this.getHistoryUrl();
     },
     methods: {
+        postHistoryUrl(e: Event) {
+            e.preventDefault();
+
+            const formData = new FormData();
+            formData.append("data", JSON.stringify({ url: this.url }));
+
+            fetch("/api/production-history/url", {
+                method: "POST",
+                headers: authHeader(),
+                body: formData,
+            }).then((response) => handleResponse(response, this.$emitter, this.$router))
+              .then((data) => {
+                  this.alertMessage = this.$t('apiresponse.' + data.code, data.param);
+                  this.alertType = data.type;
+                  this.showAlert = true;
+              });
+        },
         getHistoryUrl() {
-            // this.dataLoading = true;
-            // fetch("/api/production-history/url", { headers: authHeader() })
-            //     .then((response) => handleResponse(response, this.$emitter, this.$router))
-            //     .then((data) => {
-            //         this.url = data;
-            //         this.dataLoading = false;
-            //     });
+            this.dataLoading = true;
+            fetch("/api/production-history/url", { headers: authHeader() })
+                .then((response) => handleResponse(response, this.$emitter, this.$router))
+                .then((data) => {
+                    this.url = data;
+                })
+                .finally(() => {
+                  this.dataLoading = false;
+                })
         },
     },
 });
